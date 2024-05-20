2e3f5b9fbac82a6d95b7587f72a94441e3903f69
Charlie Giaretta <charliegiaretta@outlook.com>
2024-05-20 22:50:26 +0100

    Add further tests for UserRepository#find_by and #create

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -11,15 +11,37 @@ class UserRepository:
             for row in rows
         ]
     
+    def find(self, id):
+        rows = self._connection.execute("SELECT * FROM users WHERE id = %s", [id])
+        row = rows[0]
+        return User(
+            row['id'], row['username'], row['email'], row['password'])
+    
+    def find_by_username(self, username):
+        rows = self._connection.execute("SELECT * FROM users WHERE username = %s", (username,))
+        row = rows[0] if rows else None
+        return User(row['id'], row['username'], row['email'], row['password']) if row else None
+
+    def find_by_email(self, email):
+        rows = self._connection.execute("SELECT * FROM users WHERE email = %s", (email,))
+        row = rows[0] if rows else None
+        return User(row['id'], row['username'], row['email'], row['password']) if row else None
+
+    def find_by_email_and_password(self, email, password):
+        rows = self._connection.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        if rows:
+            row = rows[0]
+            return User(row['id'], row['username'], row['email'], row['password'])
+        return None    
+    
     def create(self, user):
+        if self.find_by_username(user.username):
+            raise ValueError("Username already exists")
+        if self.find_by_email(user.email):
+            raise ValueError("Email already exists")
         rows = self._connection.execute(
             "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
             [user.username, user.email, user.password]
         )
         return None
 
-    def find(self, id):
-        rows = self._connection.execute("SELECT * FROM users WHERE id = %s", [id])
-        row = rows[0]
-        return User(
-            row['id'], row['username'], row['email'], row['password'])
--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -1,5 +1,6 @@
 from lib.user import User
 from lib.user_repository import UserRepository
+import pytest
 
 def test_all(db_connection):
     db_connection.seed("seeds/chitter.sql")
@@ -7,6 +8,24 @@ def test_all(db_connection):
     assert repository.all() == [
         User(1, 'charliegiaretta', 'charliegiaretta@outlook.com', 'password123')
     ]
+
+def test_find_by_username(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repository = UserRepository(db_connection)
+    user = repository.find_by_username('charliegiaretta')
+    assert user == User(1, 'charliegiaretta', 'charliegiaretta@outlook.com', 'password123')
+
+def test_find_by_email(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repository = UserRepository(db_connection)
+    user = repository.find_by_email('charliegiaretta@outlook.com')
+    assert user == User(1, 'charliegiaretta', 'charliegiaretta@outlook.com', 'password123')
+
+def test_find_by_email_and_password(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repository = UserRepository(db_connection)
+    user = repository.find_by_email_and_password('charliegiaretta@outlook.com', 'password123')
+    assert user == User(1, 'charliegiaretta', 'charliegiaretta@outlook.com', 'password123')
 
 def test_create(db_connection):
     db_connection.seed("seeds/chitter.sql")
@@ -18,10 +37,18 @@ def test_create(db_connection):
         User(2, "Test username", "user@email.com", "password123")
     ]
 
-def test_find(db_connection):
+def test_create_if_username_already_exists(db_connection):
     db_connection.seed("seeds/chitter.sql")
     repository = UserRepository(db_connection)
-    album = repository.find(1)
-    assert album == User(1, 'charliegiaretta', 'charliegiaretta@outlook.com', 'password123')
+    user = User(None, "charliegiaretta", "user@email.com", "password123")
+    with pytest.raises(ValueError) as err:
+        repository.create(user)
+    assert str(err.value) == "Username already exists"
 
-    
+def test_create_if_email_already_exists(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repository = UserRepository(db_connection)
+    user = User(None, "user", "charliegiaretta@outlook.com", "password123")
+    with pytest.raises(ValueError) as err:
+        repository.create(user)
+    assert str(err.value) == "Email already exists"
